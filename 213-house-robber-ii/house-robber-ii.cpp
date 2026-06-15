be10a3fcf5256a3a3bf59dfb92a6712aa3759431
PrinceTomar1 <princetomar70276@gmail.com>
2026-06-15 13:33:13 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (73.20%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+int maximumnonadjacentsum(vector<int> &num){
+    int n = num.size();
+    int prev = num[0];
+    int prev2 = 0;
+    for(int i=1; i<n; i++){
+        int take = num[i];
+        if(i>1) take+=prev2;
+        int nottake = 0+prev;
+        int curi = max(take, nottake);
+        prev2 = prev;
+        prev = curi;
+    }
+    return prev;
+}
+    int rob(vector<int>& nums) {
+        vector<int > temp1, temp2;
+        int n = nums.size();
+        if(n==1) return nums[0];
+        for(int i=0; i<n; i++){
+            if(i!=0) temp1.push_back(nums[i]);
+            if(i!=n-1) temp2.push_back(nums[i]);
+        }
+        return max(maximumnonadjacentsum(temp1), maximumnonadjacentsum(temp2));
+    }
+};
